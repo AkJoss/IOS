a5a15d1dd87f0d29487d418f23a516a7f9239252
José Alberto Rocha Munguía <josealrocmun@gmail.com>
2026-04-22 18:47:09 -0600

    Wa

---
+++ Lúmina/Lu/Lu/HomeViewController 2.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController 2.swift
+//  Lu
+//
+//  Created by José Alberto Rocha Munguía on 15/04/26.
+//
+
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Setup the title for the navigation bar
+        self.title = "Lúmina Home"
+        print("Adri (Log): Home Screen Loaded")
+    }
+}
